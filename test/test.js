878ffda5603f83728f55701af60fa2292799dbd7
Chioma Ike <nwigwechioma@gmail.com>
2017-01-06 20:48:32 +0000

    test cases increased to 10

--- test/test.js
+++ test/test.js
@@ -17,6 +17,22 @@ var myApp = require('../app/library.js');
       it('should return [4, 6] for [6, 4]', function () {
         expect(myApp.findMinMax([6, 4])).toEqual([4, 6]);
       });
+	  
+	  it('should return [6,9] for [6, 7, 8 , 9]', function () {
+        expect(myApp.findMinMax([6, 7, 8, 9])).toEqual([6, 9]);
+      });
+	  
+	  it('should return [100,1000] for [600, 700, 100 , 1000]', function () {
+        expect(myApp.findMinMax([600, 700, 100, 1000])).toEqual([100, 1000]);
+      });
+
+      it('should return [2, 4] for [4, 2]', function () {
+        expect(myApp.findMinMax([4, 2])).toEqual([2, 4]);
+      });
+	  
+	  it('should return [4, 65] for [4, 65, 8, 44, 9, 8, 60]', function () {
+        expect(myApp.findMinMax([4, 65, 8, 44, 9, 8, 60])).toEqual([4, 65]);
+      });
 
       it('should return [2, 78] for [4, 66, 6, 44, 7, 78, 8, 68, 2]', function () {
         expect(myApp.findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).toEqual([2, 78]);
@@ -28,6 +44,14 @@ var myApp = require('../app/library.js');
 
       it('should return [4] for [4, 4, 4, 4]', function () {
         expect(myApp.findMinMax([4, 4, 4, 4])).toEqual([4]);
+      });
+	  
+	  it('should return [6] for [6, 6, 6, 6]', function () {
+        expect(myApp.findMinMax([6, 6, 6, 6])).toEqual([6]);
+      });
+	  
+	  it('should return [11] for [11, 11, 11, 11]', function () {
+        expect(myApp.findMinMax([11, 11, 11, 11])).toEqual([11]);
       });
 
     });
